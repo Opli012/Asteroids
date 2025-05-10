f54e6f3b45d4b2bbcbd348cddf3ce010d93fbdd4
József <opli012gm@gmail.com>
2025-05-10 23:48:32 +0200

    Minor reorganization

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-
-    player = Player(x, y)
-
-    while 1 == 1:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        delta = clock.tick(60)
-        dt = delta / 1000
 
         screen.fill("black")
-
         player.draw(screen)
-
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
