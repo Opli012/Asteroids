4ffca7c2ee15747e24f2f18af4f32af61b185a07
József <opli012gm@gmail.com>
2025-05-11 18:46:59 +0200

    Asteroid field is implemented

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
-    updatables = pygame.sprite.Group()
-    drawables = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -20,11 +28,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
-        updatables.update(dt)
+        updatable.update(dt)
 
-        for drawable in drawables:
-            drawable.draw(screen)
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
